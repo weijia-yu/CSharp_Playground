0b43e25c4fb217541d6581c0cb8e2c4d7e94a4e6
weijia yu <weijia@oben.com>
2019-01-25 14:17:27 -0800

    add async sample

---
+++ Cs_Playground/Async/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cs_Playground
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("before first task!");
+            LoadMesh();
+                              Console.WriteLine("after first task!");
+            Thread.Sleep(7000);
+        }
+
+        private static async Task LoadMesh() {
+            await LoadCustomHeadsFromOBN();
+            Console.WriteLine("continue ClearHeadConstructionBlendshape");
+        }
+
+        private static async Task LoadCustomHeadsFromOBN() {
+            await LoadMeshesFromOBN();
+        }
+
+
+        private static async Task LoadMeshesFromOBN() {
+            //Thread.Sleep(5000);
+            await Task.Run(() =>
+            {
+                //Heacy calculation
+                Thread.Sleep(6000);
+            });
+            Console.WriteLine("calculation finishes!");
+        }
+
+
+
+    }
+}
